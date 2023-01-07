cb97381bff9b876e71335f8524cd075869f91af2
bans <buyan.fred@gmail.com>
2023-01-07 21:06:14 +0400

    Changes with configurations

--- _Tests/_base_test.py
+++ _Tests/_base_test.py
@@ -1,21 +1,24 @@
 import unittest
 from selenium import webdriver
+from resources.configuration import Configurations as cfg
 
 
 class BaseTest(unittest.TestCase):
+
+    driver = None
+    chrome_options = None
 
     @classmethod
     def setUpClass(cls):
         cls.chrome_options.add_argument("--no-sandbox")
         cls.chrome_options.add_argument("--start-maximized")
         cls.chrome_options.add_argument("--disable-dev-shm-usage")
-        # cls.chrome_options.add_argument("--headless")
-
+        if cfg.HEADLESS:
+            cls.chrome_options.add_argument("--headless")
         cls.driver = webdriver.Chrome(options=cls.chrome_options)
         cls.driver.implicitly_wait(10)
-
     @classmethod
     def tearDownClass(cls):
-        cls.driver.close()
-        cls.driver.quit()
-        print("OK!")
+        if not cfg.HOLD_BROWSER_OPEN:
+            cls.driver.close()
+            cls.driver.quit()
